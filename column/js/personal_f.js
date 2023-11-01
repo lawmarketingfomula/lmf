bbbb26ff8dae1b8d561b74cd34b2c9cf2bcae1f0
lawmarketingfomula <149296279+lawmarketingfomula@users.noreply.github.com>
2023-11-01 10:45:18 +0900

    Add files via upload

--- column/js/personal_f.js
+++ column/js/personal_f.js
@@ -13,23 +13,7 @@ $(function(){
 
    });
 
-     // 현재 날짜 정보 가져오기
-  const today = new Date();
+  
 
-  // 연도, 월, 일, 요일 정보 가져오기
-  const year = today.getFullYear();
-  const month = today.getMonth() + 1;
-  const lastDay = new Date(year, month, 0).getDate();
-
-  // 요일을 한국어로 표시
-  const daysOfWeek = ['일', '월', '화', '수', '목', '금', '토'];
-  const dayOfWeek = daysOfWeek[today.getDay()];
-
-  // 결과를 표시할 요소 선택
-  const dateInfo = document.getElementById('dateInfo');
-
-  // 결과 표시
-  dateInfo.innerHTML = `✅ 신청기간 : ${year}년. ${month}. ${lastDay} (${dayOfWeek}) 모집 종료`;
-    
 })
 
